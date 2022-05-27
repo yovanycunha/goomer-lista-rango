5c9bcdeeb51b45192ce46cb7df895b09afd61f66
Yovany Cunha <yovany33@gmail.com>
2022-05-27 16:38:33 -0300

    restaurant page structure

--- src/pages/[restaurant]/[id].page.tsx
+++ src/pages/[restaurant]/[id].page.tsx
@@ -11,21 +11,48 @@ import style from './Restaurant.module.scss';
 function Restaurant() {
   const { allRestaurants } = useRestaurantList();
   const router = useRouter();
-  const [restaurant, setRestaurant] = useState<TRestaurant>(
-    allRestaurants.filter((r) => String(r.id) === router!.query!.id)[0]
-  );
-  const [loading, toggleLoading] = useReducer((l) => !l, true);
+  const [restaurant, setRestaurant] = useState<TRestaurant>();
+  // const [loading, toggleLoading] = useReducer((l) => !l, true); usar no modal
+
+  const renderOpeningHours = () => {
+    const days = [];
+  };
+
+  useEffect(() => {
+    setRestaurant(
+      allRestaurants.filter((r) => String(r.id) === router!.query!.id)[0]
+    );
+  }, [allRestaurants, router]);
 
   return (
-    <div className={style.container}>
-      {loading && <Loading />}
-      {/* <Image
-        src={restaurant!.image}
-        alt="Restaurant Image"
-        width={100}
-        height={100}
-      /> */}
-    </div>
+    <>
+      <div className={style.topbar} />
+      <div className={style.container}>
+        {!restaurant ? (
+          <Loading />
+        ) : (
+          <>
+            <div className={style.infoContainer}>
+              <Image
+                src={restaurant.image}
+                alt="Restaurant Image"
+                width={100}
+                height={100}
+              />
+              <div className={style.info}>
+                <h1>{restaurant.name}</h1>
+                <span>{restaurant.address}</span>
+              </div>
+            </div>
+
+            <div className={style.content}>
+              <input type="search" />
+            </div>
+            <div>CARDÁPIO</div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
